c855d8103cb39f22fb025e876e2851ca0b3e8483
amir <kmuc72@gmail.com>
2019-01-05 03:10:40 +0100

    deleted files

--- shopify-api-node-example/index.js
+++
@@ -1,8 +0,0 @@
-const Shopify = require('shopify-api-node');
-
-const shopify = new Shopify({
-    shopName: 'your-shop-name',
-    accessToken: 'your-oauth-token'
-});
-
-shopify.on('callLimits', limits => console.log(limits));
