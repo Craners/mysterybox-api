6e8f1979ba656e418ab1610983bcf26558165eb2
amir <kmuc72@gmail.com>
2019-03-03 17:28:42 +0100

    removed unused package

--- after-sale/src/getdata/getData.module.ts
+++ after-sale/src/getdata/getData.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GetDataController } from './getData.controller';
 import { GetDataService } from './getData.service';
-import { DatabaseModule } from 'dist/src/database/database.module';
 import { ShopModule } from 'src/shop/shop.module';
 
 @Module({
-    imports: [DatabaseModule, ShopModule],
+    imports: [ ShopModule],
     controllers: [GetDataController],
     providers: [GetDataService]
 })
