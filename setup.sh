8b21f6a524df5c9f7f4e0d3ae419b0f587b14780
codacy20 <kmuc72@gmail.com>
2019-06-23 15:46:15 +0200

    config test 11th try

--- setup.sh
+++ setup.sh
@@ -9,6 +9,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'
 sudo apt update
 apt-cache policy docker-ce
-sudo apt-get install docker-engine
-sudo apt install docker-ce
+apt-get -f install docker-ce
 sudo systemctl status docker
