5aebaed68c929478d37191214a79062e29b5b419
codacy20 <kmuc72@gmail.com>
2019-06-23 14:57:23 +0200

    config test 8th try

---
+++ setup.sh
@@ -0,0 +1,13 @@
+ls 
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get clean
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get upgrade
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
